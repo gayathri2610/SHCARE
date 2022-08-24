6b6d1b59469757d1c2e7f3ced2e8dde3fa0f2ca5
gayathri2610 <yanakandlagayathri26@gmail.com>
2022-08-24 12:44:18 +0530

    home and navigation

--- frontend/src/Components/Navigationbar.js
+++ frontend/src/Components/Navigationbar.js
@@ -4,49 +4,50 @@ const Navigationbar = () => {
   return (
     <div>
       {" "}
-<<<<<<< HEAD
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark ">
-=======
-      <nav className="navbar navbar-expand-lg"
-      style={{
-        backgroundColor:"#1D5C63"
-      }}
-      >
->>>>>>> 019916f85dd6543627855a7268b989798c0b8f61
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"></a>
-          <button
-            className="navbar-toggler "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav" 
-            style={{
-                textColor:"black"
-              }}>
-              <a className="nav-link active" aria-current="page" href="/h">
-                Home
-              </a>
-              <a className="nav-link" href="/tracker">
-                Tracker
-              </a>
+        <nav
+          className="navbar navbar-expand-lg"
+          style={{
+            backgroundColor: "#1D5C63",
+          }}
+        >
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#"></a>
+            <button
+              className="navbar-toggler "
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div
+                className="navbar-nav"
+                style={{
+                  textColor: "black",
+                }}
+              >
+                <a className="nav-link active" aria-current="page" href="/h">
+                  Home
+                </a>
+                <a className="nav-link" href="/tracker">
+                  Tracker
+                </a>
 
-              <a className="nav-link" href="Aboutus">
-                About Us
-              </a>
-              <a className="nav-link" href="/login">
-                Login
-              </a>
+                <a className="nav-link" href="Aboutus">
+                  About Us
+                </a>
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </nav>
       </nav>
     </div>
   );
